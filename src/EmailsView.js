088b59a4172c5e8149be6d847d06b0b4c0c61a03
ritikavermaa24 <45839582+ritikavermaa24@users.noreply.github.com>
2021-04-09 19:51:46 +0530

    change

--- src/EmailsView.js
+++ src/EmailsView.js
@@ -1,11 +1,64 @@
 import React from 'react'
+import styled from 'styled-components'
 
-function EmailsView() {
+import Checkbox from '@material-ui/core/Checkbox'
+import RefreshIcon from '@material-ui/icons/Refresh';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import IconButton from '@material-ui/core/IconButton';
+
+import { emailData } from './data/EmailData';
+
+import EmailItem from './EmailItem';
+
+const EmailsView = () => {
     return (
-        <div>
-            <h1> fhfjhg</h1>
-        </div>
+        <Wrapper>
+            <TopWrapper>
+                <Checkbox />
+                
+                <IconButton>
+                    <RefreshIcon />
+                </IconButton>
+                
+                <IconButton>
+                    <MoreVertIcon />
+                </IconButton>
+            </TopWrapper>
+
+            <EmailsContainer>
+                {
+                    emailData.map(({starred, from, subject, message, received, read})=>(
+                        <EmailItem
+                            starred={starred}
+                            from={from}
+                            subject={subject}
+                            message={message}
+                            received={received}
+                            read={read}
+                        />
+                        
+                    ))
+                }
+            </EmailsContainer>
+
+           
+        </Wrapper>
     )
 }
 
 export default EmailsView
+
+const Wrapper = styled.div``
+
+const TopWrapper = styled.div`
+height: 48px;
+display: flex;
+padding-left: 20px;
+
+
+    
+    
+`
+
+const EmailsContainer = styled.div`
+`
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,82 +3,130 @@ import styled from 'styled-components';
 import Compose from './Components/buttons/Compose';
 import { sidebarButtonItems} from "./data/SidebarButtonItems";
 
-function Sidebar() {
+import VideocamIcon from '@material-ui/icons/Videocam';
+import KeyboardIcon from '@material-ui/icons/Keyboard';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+import { bottomIcons } from "./data/BottomIconData";
+
+
+const Sidebar = () => {
     return (
         <Wrapper>
-            <ComposeWrapper>
-                <Compose/>
-            </ComposeWrapper>
+            <TopSectionWrapper>
+                <ComposeWrapper>
+                    <Compose />
+                </ComposeWrapper>
 
+                <SideButtonsWrapper>
+                    {
+                        sidebarButtonItems.map(item =>(
+                            <SidebarButtonItem>{item.icon} {item.text}</SidebarButtonItem>
+                        ))
+                    }
+                </SideButtonsWrapper>
+            </TopSectionWrapper>
 
-            <SideButtonWrapper>
-                {
-                    sidebarButtonItems.map(item => (
-                        <SidebarButtonItem>
-                            {item.icon} {item.text}
-                        </SidebarButtonItem>
-                    ))
-                }
+            <BottomSectionWrapper>
+                <SidebarSectionWrapper>
+                    <Title>Meet</Title>
+                    <p><VideocamIcon/> New Meeting</p>
+                    <p><KeyboardIcon/> Join Meeting</p>
+                </SidebarSectionWrapper>
 
-            </SideButtonWrapper>
+                <SidebarSectionWrapper>
+                    <Title>Hangouts</Title>
+                    <p><AccountCircleIcon/> David Rakosi</p>
+                </SidebarSectionWrapper>
 
-            
-            <MeetWrapper>
-
-            </MeetWrapper>
-
-
-            <HangoutsWrapper>
-
-            </HangoutsWrapper>
-
-
-            <BottomIconsWrapper>
-
-            </BottomIconsWrapper>
+                <BottomIconsWrapper>
+                    {
+                        bottomIcons.map(icon => (
+                            <>
+                                {icon}
+                            </>
+                        ))
+                    }
+                </BottomIconsWrapper>
+            </BottomSectionWrapper>
 
         </Wrapper>
     )
 }
 
-export default Sidebar;
+export default Sidebar
 
 const Wrapper = styled.div`
     border-right: 1px solid lightgray;
-    height: 100vh;
+    height: calc(100vh - 70px);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 `
+
+const BottomSectionWrapper = styled.div`
+    margin-bottom: 30px;
+`
+
+const TopSectionWrapper = styled.div``
 
 const ComposeWrapper = styled.div`
     display: grid;
-    place-item: start;
+    place-items: start;
     padding: 10px 20px;
+
 
 `
 
-const SideButtonWrapper = styled.div``
+const SideButtonsWrapper = styled.div``
 
 const SidebarButtonItem = styled.div`
     display: grid;
-    
-    grid-template-columns: 14% 14%;
+    grid-template-columns: 14% 14%; 
     color: gray;
     padding: 5px 25px;
     border-radius: 0 100px 100px 0;
     cursor: pointer;
-    margin-right: 8px;
-    letter-spacing: 0.5px;
-
+    
+    :hover{
+        background-color: #f5f7f7
+    }
     
 
-    :hover{
-        background-color: #f5f7f7;
-    }
+
 `
 
-const MeetWrapper = styled.div``
+const SidebarSectionWrapper = styled.div`
+    border-top: 1px solid lightgray;
+    p {
+        color: gray;
+        display: grid;
+        grid-template-columns: 14% auto;
+        height: 30px;
+        align-items: center;
+        padding-left: 25px;
+        padding-right: 100px;
+        margin-top: 5px;
+        margin: 0px;
+    }
 
-const HangoutsWrapper = styled.div``
+`
 
-const BottomIconsWrapper = styled.div``
+const Title = styled.h4`
+    
+    padding-left: 25px;
+    margin-bottom: 4px;
+    margin-top: 8px;
+    margin-right: 190px;
+`
 
-
+const BottomIconsWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    color: gray;
+    border-top: 1px solid lightgray;
+    margin-top: 60px;
+    .MuiSvgIcon-root{
+        padding: 2px;
+    }
+`
